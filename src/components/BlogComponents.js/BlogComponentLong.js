2774520c16e66ed8db4d9594264ff205ca7b55fc
Starko00 <stefanvukmanovic77@gmail.com>
2023-02-02 00:11:32 +0100

    blog

--- src/components/BlogComponents.js/BlogComponentLong.js
+++ src/components/BlogComponents.js/BlogComponentLong.js
@@ -22,15 +22,8 @@ export const BlogComponentLong = () => {
           src={"http://www.with-us.me/static/blogImgs/" + blogData[0]?.img}
         />
       </div>
-      <h1>{blogData[0]?.tittle}</h1>
+
       <div className={style.innrContainer}>
-        <div className={style.listHolder}>
-          <ul>
-            <li>Blog 1</li>
-            <li>Blog 2</li>
-            <li>Blog 3</li>
-          </ul>
-        </div>
         <div dangerouslySetInnerHTML={{ __html: blogData[0]?.text }}></div>
       </div>
     </div>
