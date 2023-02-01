78936050cfe00218c5b2193d31903656d65c2b20
Starko00 <stefanvukmanovic77@gmail.com>
2023-02-01 11:49:39 +0100

    Blog loading

--- src/components/BlogComponents.js/BlogComponentLong.js
+++ src/components/BlogComponents.js/BlogComponentLong.js
@@ -1,25 +1,38 @@
-import axios from "axios"
-import BlogComponentLongStyle from './BlogComponentLongStyle.module.css'
-import { useState, useEffect } from "react"
-import {useLocation} from 'react-router-dom';
-export const BlogComponentLong = ()=>{
-    const {state} = useLocation()
-    const style = BlogComponentLongStyle
-    console.log(state)
-    return <div className={style.container}>
-        <div className={style.imgHolder}><img src={'http://www.with-us.me/static/blogImgs/'+state?.img} /></div>
-        <h1>{state?.tittle}</h1>
-        <div className={style.innrContainer}>
+import axios from "axios";
+import BlogComponentLongStyle from "./BlogComponentLongStyle.module.css";
+import { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
+export const BlogComponentLong = () => {
+  const [blogData, setBlogData] = useState("");
+  const { state } = useLocation();
+  const style = BlogComponentLongStyle;
+  const { id } = useParams();
+  useEffect(() => {
+    axios
+      .post("http://www.with-us.me/api/withus/v1/blog", { tittle: id })
+      .then((res) => {
+        setBlogData(res.data.blog);
+      });
+  }, [id]);
+
+  return (
+    <div className={style.container}>
+      <div className={style.imgHolder}>
+        <img
+          src={"http://www.with-us.me/static/blogImgs/" + blogData[0]?.img}
+        />
+      </div>
+      <h1>{blogData[0]?.tittle}</h1>
+      <div className={style.innrContainer}>
         <div className={style.listHolder}>
-            <ul>
-                <li>Blog 1</li>
-                <li>Blog 2</li>
-                <li>Blog 3</li>
-            </ul>
+          <ul>
+            <li>Blog 1</li>
+            <li>Blog 2</li>
+            <li>Blog 3</li>
+          </ul>
         </div>
-        <div  dangerouslySetInnerHTML={{__html:state?.text}}>
-        </div>
-        </div>
-        
+        <div dangerouslySetInnerHTML={{ __html: blogData[0]?.text }}></div>
+      </div>
     </div>
-}
+  );
+};
