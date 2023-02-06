8110717245435ce036a25c52499d0acb86932467
D-e-D-a <100025592+D-e-D-a@users.noreply.github.com>
2023-02-06 20:25:59 +0100

    small fix

--- src/components/ServicesComponents/CardsData.js
+++ src/components/ServicesComponents/CardsData.js
@@ -12,7 +12,7 @@ export const cardData = [
   {
     id: 3,
     bg: "https://images.unsplash.com/photo-1654250910768-0162e080ef86?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
-    header: "User basedUI/UX Design",
+    header: "User based UI/UX Design",
   },
   {
     id: 4,
